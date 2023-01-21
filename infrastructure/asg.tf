d7aa24b9cbe9affaa004583569c9755061b19461
Uliana Neiliuk <60117819+uliananeil@users.noreply.github.com>
2023-01-21 19:34:51 +0200

    Update asg.tf

--- infrastructure/asg.tf
+++ infrastructure/asg.tf
@@ -7,22 +7,6 @@ resource "aws_appautoscaling_target" "ecs_target" {
   service_namespace  = "ecs"
 }
 
-resource "aws_appautoscaling_policy" "ecs_policy_memory" {
-  name               = "django-memory-autoscaling"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-
-    target_value = 90
-  }
-}
-
 resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
   name               = "django-cpu-autoscaling"
   policy_type        = "TargetTrackingScaling"
